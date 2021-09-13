e82b438d2ebdce0421685dd48faa7aebaeb61695
PascalVALLADE <shoshoie@free.fr>
2021-09-13 19:57:17 +0200

    reftech ok

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,7 +4,13 @@ import pascal from 'src/components/Home/images/pascal.png';
 import personnalite from 'src/assets/images/personnalite.png';
 import poker from 'src/components/Home/images/poker.png';
 import parcours from 'src/components/Home/images/parcours.png';
-// test
+import react from 'src/components/Home/images/react.svg';
+import html from 'src/components/Home/images/html.svg';
+import css from 'src/components/Home/images/CSS.svg';
+import sass from 'src/components/Home/images/sass.svg';
+import redux from 'src/components/Home/images/redux.png';
+import postgres from 'src/components/Home/images/postgres.svg';
+
 const Home = () => (
   <div className="mainHome">
     <div className="mainPresentation">
@@ -24,35 +30,49 @@ const Home = () => (
       </div>
 
       <div className="containerBtn">
-        <a className="btn" href="/cv" target="_blank">
+        <section className="btn" href="/cv" target="_blank">
           <p className="titre">
             MON PARCOURS PROFESSIONNEL
           </p>
           <p className="resume">
             Aprés une belle carrière dans l'expertise fonctionnelle applicative, je me consacre désormais au développement web. Mes motivations restent identiques : proposer un service de qualité aux utilisateurs.
           </p>
-          <img className="image" src={parcours} alt="MON PARCOURS PROFESSIONNEL" />
-        </a>
+          <div className="btn__refTech">
+            <img className="btn__refTech__image2" src={react} alt="logo_réact" />
+            <img className="btn__refTech__image2" src={sass} alt="logo_sass" />
+            <img className="btn__refTech__image1" src={parcours} alt="MON PARCOURS PROFESSIONNEL" />
+          </div>
+          {/* <img className="image" src={parcours} alt="MON PARCOURS PROFESSIONNEL" /> */}
+        </section>
 
-        <a className="btn" href="https://pv-personnality.herokuapp.com/" target="_blank">
+        <section className="btn" href="https://pv-personnality.herokuapp.com/" target="_blank">
           <p className="titre">
             MAIS QUI SUIS-JE ?
           </p>
           <p className="resume">
             Vous souhaitez en apprendre davantage sur ma personnalié ? Je vous invite à consulter ce graphe intéractif.
           </p>
-          <img className="image" src={personnalite} alt="Qui suis-je ?" />
-        </a>
+          <div className="btn__refTech">
+            <img className="btn__refTech__image2" src={html} alt="logo_html" />
+            <img className="btn__refTech__image2" src={css} alt="logo_css" />
+            <img className="btn__refTech__image1" src={personnalite} alt="Qui suis-je ?" />
+          </div>
+        </section>
 
-        <a className="btn" href="https://pokertool.herokuapp.com/" target="_blank">
+        <section className="btn" href="https://pokertool.herokuapp.com/" target="_blank">
           <p className="titre">
             PROJET : POKER-TOOL
           </p>
           <p className="resume">
             Poker-tool est une application conçue durant ma formation de "Déloppeur Web Fullstack". Elle s'adresse à tous ceux recherchant un outil afin de gérer un tournoi entre amis.
           </p>
-          <img className="image" src={poker} alt="PROJET : POKER-TOOL" />
-        </a>
+          <div className="btn__refTech">
+            <img className="btn__refTech__image2" src={redux} alt="logo_redux" />
+            <img className="btn__refTech__image2" src={postgres} alt="logo_postgres" />
+            <img className="btn__refTech__image1" src={poker} alt="PROJET : POKER-TOOL" />
+          </div>
+
+        </section>
       </div>
     </div>
   </div>
