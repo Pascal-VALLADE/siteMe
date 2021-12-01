c30838a409b0b3824a32156552c71a8c285e3d09
PascalVALLADE <shoshoie@free.fr>
2021-12-01 14:23:17 +0100

    is OK

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import 'src/components/Home/home.scss';
-import personnalite from 'src/assets/images/personnalite.png';
-import poker from 'src/components/Home/images/poker.png';
+import personnalite from 'src/components/Home/images/personnality.png';
+import poker from 'src/components/Home/images/logo_noir.svg';
 import parcours from 'src/components/Home/images/parcours.png';
-import react from 'src/components/Home/images/react.svg';
+import react from 'src/components/Home/images/react.png';
 import html from 'src/components/Home/images/html.svg';
 import css from 'src/components/Home/images/CSS.svg';
 import sass from 'src/components/Home/images/sass.svg';
 import redux from 'src/components/Home/images/redux.png';
-import postgres from 'src/components/Home/images/postgres.svg';
+import postgres from 'src/components/Home/images/postgres.png';
 import js from 'src/components/Home/images/js.png';
 import nodejs from 'src/components/Home/images/Nodejs.png';
 
 const Home = () => (
   <div className="containerBtn">
     <a className="btn" href="/cv" target="_blank">
-      <p className="titre">MON PARCOURS PROFESSIONNEL</p>
+      <p className="titre">MON PARCOURS...</p>
       <p className="resume">
         Après une belle carrière dans l'expertise fonctionnelle applicative, je
         me consacre désormais au développement web. Mes motivations restent
@@ -25,11 +25,11 @@ const Home = () => (
         <img className="btn__refTech__image2" src={js} alt="logo_js" />
         <img className="btn__refTech__image2" src={react} alt="logo_réact" />
         <img className="btn__refTech__image2" src={sass} alt="logo_sass" />
-        <img
+        {/* <img
           className="btn__refTech__image1"
           src={parcours}
           alt="MON PARCOURS PROFESSIONNEL"
-        />
+        /> */}
       </div>
     </a>
 
@@ -47,11 +47,11 @@ const Home = () => (
       <div className="btn__refTech">
         <img className="btn__refTech__image2" src={html} alt="logo_html" />
         <img className="btn__refTech__image2" src={css} alt="logo_css" />
-        <img
+        {/* <img
           className="btn__refTech__image1"
           src={personnalite}
           alt="Qui suis-je ?"
-        />
+        /> */}
       </div>
     </a>
 
@@ -68,10 +68,12 @@ const Home = () => (
       <div className="btn__refTech">
         <img className="btn__refTech__image2" src={js} alt="logo_js" />
         <img className="btn__refTech__image2" src={react} alt="logo_réact" />
-        <img className="btn__refTech__image2" src={redux} alt="logo_redux" />
+      </div>
+      <div className="btn__refTech">
+        {/* <img className="btn__refTech__image2" src={redux} alt="logo_redux" /> */}
         <img className="btn__refTech__image2" src={nodejs} alt="logo_nodejs" />
         <img className="btn__refTech__image2" src={postgres} alt="logo_postgres" />
-        <img className="btn__refTech__image1" src={poker} alt="PROJET : POKER-TOOL" />
+        {/* <img className="btn__refTech__image1" src={poker} alt="PROJET : POKER-TOOL" /> */}
       </div>
     </a>
   </div>
